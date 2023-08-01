c84224b9c5e356bd2d62a782941ded1265dde22a
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-08-01 07:31:49 -0700

    making use of swiper for my head section

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+ // Header Scroll
+
+ // Nav hid
+
+ //swiper slider
+ const swiper = new Swiper('.mySwiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3500,
+    },
+  });
+  
+
+ //counter design
+
+ //our partner
