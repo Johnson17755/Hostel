14605e5ef1e167332247be5e90c43e5a112cc34a
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-08-02 13:15:05 -0700

    updating

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,20 @@
  // Header Scroll
-
+const nav = document.querySelector('.navbar');
+window.onscroll = function () {
+  if(document.documentElement.scrollTop > 50){
+    nav.classList.add('header-scrolled')
+  }else{
+    nav.classList.remove('header-scrolled')
+  }
+}
  // Nav hid
-
+ const navBar = document.querySelectorAll('.nav-link');
+ const navCollapse  = document.querySelector('.navbar-collapse.collapse');
+ navBar.forEach(function(a){
+    a.addEventListener('click', function(){
+      navCollapse.classList.remove('show');
+    })
+ });
  //swiper slider
  const swiper = new Swiper('.mySwiper', {
     direction: 'vertical',
