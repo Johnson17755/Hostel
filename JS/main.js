ba0aedf5306bc03663d403283f57dc66c8c1cd11
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-08-09 22:45:15 -0700

    updating and optimisation

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,9 @@
 const nav = document.querySelector('.navbar');
 window.onscroll = function () {
   if(document.documentElement.scrollTop > 50){
-    nav.classList.add('header-scrolled')
+    nav.classList.add('header-scrolled');
   }else{
-    nav.classList.remove('header-scrolled')
+    nav.classList.remove('header-scrolled');
   }
 }
  // Nav hid
@@ -16,7 +16,7 @@ window.onscroll = function () {
     })
  });
  //swiper slider
- const swiper = new Swiper('.mySwiper', {
+ var swiper = new Swiper('.mySwiper', {
     direction: 'vertical',
     loop: true,
     pagination: {
@@ -51,3 +51,27 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
  //our partner
+ var swiper = new Swiper('.our-partner', {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+  },
+});
+
+
